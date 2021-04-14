44d1ae87a20c8586363db18cde1cbefe5cde1b5b
Quinn <quinn@indra.ai>
2021-04-14 16:52:48 -0700

    column list buttons

--- src/utils.c
+++ src/utils.c
@@ -998,7 +998,7 @@ void column_list(struct char_data *ch, int num_cols, const char **list, int list
        if (offset < list_length)
        {
          if (show_nums)
-           temp_len = snprintf(buf+len, sizeof(buf) - len, "-:mudder[%s]:%d:-", list[(offset)], offset+1);
+           temp_len = snprintf(buf+len, sizeof(buf) - len, "\nconfirm[%s]:%d\n\r", list[(offset)], offset+1);
          else
            temp_len = snprintf(buf+len, sizeof(buf) - len, "%-*s", col_width, list[(offset)]);
          len += temp_len;
