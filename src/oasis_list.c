148854bab6759928548f09541472d186aa505768
Quinn <quinn@indra.ai>
2021-04-21 15:06:55 -0700

    lists

--- src/oasis_list.c
+++ src/oasis_list.c
@@ -785,8 +785,8 @@ static void list_zones(struct char_data *ch, zone_rnum rnum, zone_vnum vmin, zon
     bool zname = (!use_name) || (is_name(name, zone_table[i].builders));
     if (znum && zname) {
       counter++;
-      tmp_len = snprintf(buf+len, sizeof(buf)-len, "%d] %-*s %s%-1s%s\r\n",
-          zone_table[i].number, count_color_chars(zone_table[i].name)+30, zone_table[i].name,
+      tmp_len = snprintf(buf+len, sizeof(buf)-len, "%d] %s%-1s%s\r\n",
+          zone_table[i].number, zone_table[i].name,
           QYEL, zone_table[i].builders ? zone_table[i].builders : "None.", QNRM);
       len += tmp_len;
       if (len > sizeof(buf))
