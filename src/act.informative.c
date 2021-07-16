5f72668d315fe02f4cffd31020a1855f35c672ff
Quinn <quinn@indra.ai>
2021-07-15 21:20:12 -0700

    updating informative.c

--- src/act.informative.c
+++ src/act.informative.c
@@ -687,9 +687,9 @@ static void look_at_target(struct char_data *ch, char *arg)
     send_to_char(ch, "\n# %s\r"
     "\nadv:world:%d/%s\r"
     "\n \r",
-    obj->description,
+    ch->description,
     GET_ROOM_VNUM(IN_ROOM(ch)),
-    obj->description
+    ch->description
   );
     // page_string(ch->desc, desc, FALSE);
     return;
