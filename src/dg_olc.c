fd7bf91c54b7440196a04fd4aa630e334ad89ce5
Quinn <quinn@indra.ai>
2021-07-16 22:32:31 -0700

    removing syntax highlighting

--- src/dg_olc.c
+++ src/dg_olc.c
@@ -262,61 +262,6 @@ static void trigedit_disp_types(struct descriptor_data *d)
 
 }
 
-/****************************************************************************************
- DG Scripts Code Syntax Highlighting
- Created by Victor Almeida (aka Stoneheart) in Brazil
- from BrMUD:Tormenta www.tormenta.com.br
-
- License: Attribution 4.0 International (CC BY 4.0)
- http://creativecommons.org/licenses/by/4.0/
-
- You are free to:
- Share — copy and redistribute the material in any medium or format
- Adapt — remix, transform, and build upon the material for any purpose, even commercially.
-
- The licensor cannot revoke these freedoms as long as you follow the license terms.
-
- Under the following terms:
- Attribution — You must give appropriate credit, provide a link to the license, and indicate
- if changes were made. You may do so in any reasonable manner, but not in any way that
- suggests the licensor endorses you or your use.
- *****************************************************************************************/
-
-// Change a string for another without memory bugs
-static char *str_replace(const char *string, const char *substr, const char *replacement) {
-    char *tok = NULL;
-    char *newstr = NULL;
-    char *oldstr = NULL;
-    char *head = NULL;
-
-    // if either substr or replacement is NULL, duplicate string a let caller handle it
-    if (substr == NULL || replacement == NULL) {
-        return strdup (string);
-    }
-
-    newstr = strdup (string);
-    head = newstr;
-    while ((tok = strstr(head, substr))) {
-        oldstr = newstr;
-        newstr = malloc(strlen(oldstr) - strlen(substr) + strlen (replacement) + 1);
-        // Failed to alloc mem, free old string and return NULL
-        if (newstr == NULL) {
-            free (oldstr);
-            return NULL;
-        }
-        memcpy (newstr, oldstr, tok - oldstr);
-        memcpy (newstr + (tok - oldstr), replacement, strlen (replacement));
-        memcpy (newstr + (tok - oldstr) + strlen(replacement), tok + strlen (substr), strlen (oldstr) - strlen (substr) - (tok - oldstr));
-        memset (newstr + strlen (oldstr) - strlen(substr) + strlen (replacement) , 0, 1 );
-
-        // move back head right after the last replacement
-        head = newstr + (tok - oldstr) + strlen( replacement);
-        free (oldstr);
-    }
-
-    return newstr;
-}
-
 void trigedit_parse(struct descriptor_data *d, char *arg)
 {
   int i = 0;
