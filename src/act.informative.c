705268b007e2a9613a57627706e83e8d9be35b1f
Quinn <quinn@indra.ai>
2021-07-17 22:46:32 -0700

    testing extra items template load

--- src/act.informative.c
+++ src/act.informative.c
@@ -716,13 +716,13 @@ static void look_at_target(struct char_data *ch, char *arg)
 
   /* Does the argument match an extra desc of an object in the room? */
   for (obj = world[IN_ROOM(ch)].contents; obj && !found; obj = obj->next_content) {
-    send_to_char(ch, "\nfound extra: %s", find_exdesc(arg, obj->ex_description));
     if (CAN_SEE_OBJ(ch, obj)) {
       if ((desc = find_exdesc(arg, obj->ex_description)) != NULL && ++i == fnum) {
         send_to_char(ch, "\n%s\r", desc);
 	      found = TRUE;
       }
     }
+    send_to_char(ch, "\nfound extra: %s", find_exdesc(arg, obj->ex_description));
   }
 
   /* If an object was found back in generic_find */
