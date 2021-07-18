d440f031121056bb2371734aa080fc0df80092df
Quinn <quinn@indra.ai>
2021-07-17 22:53:52 -0700

    working on look at informative

--- src/act.informative.c
+++ src/act.informative.c
@@ -687,7 +687,7 @@ static void look_at_target(struct char_data *ch, char *arg)
     send_to_char(ch, "\n# Look\r"
     "\nadv:world:%d/%s\r"
     "\n=\n"
-    "room:%d",
+    "\nroom:%d\r",
     GET_ROOM_VNUM(IN_ROOM(ch)),
     arg,
     GET_ROOM_VNUM(IN_ROOM(ch))
