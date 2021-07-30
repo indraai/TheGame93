2bbf24d93d4e9f6c1b2482532d8e3c379e08235d
Quinn <quinn@indra.ai>
2021-07-29 23:17:45 -0700

    cleaning up line break on agent informative

--- src/act.informative.c
+++ src/act.informative.c
@@ -331,7 +331,7 @@ static void list_one_char(struct char_data *i, struct char_data *ch)
         send_to_char(ch, "(Blue Aura) ");
       }
     }
-    send_to_char(ch, "agent: %s", i->player.long_descr);
+    send_to_char(ch, "\nagent: %s", i->player.long_descr);
 
     if (AFF_FLAGGED(i, AFF_SANCTUARY))
       act("...$e glows with a bright light!", FALSE, i, 0, ch, TO_VICT);
