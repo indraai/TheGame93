79c026a7492cb5ee67fa8ffc328f4147e3a4fbef
Quinn <quinn@indra.ai>
2021-09-18 18:56:42 -0700

    config.c

--- src/config.c
+++ src/config.c
@@ -170,19 +170,19 @@ int selfdelete_fastwipe = YES;
 
 /* ROOM NUMBERS */
 /* Virtual number of room that mortals should enter at. */
-room_vnum mortal_start_room = 0;
+room_vnum mortal_start_room = 2;
 
 /* Virtual number of room that immorts should enter at by default. */
-room_vnum immort_start_room = 0;
+room_vnum immort_start_room = 2;
 
 /* Virtual number of room that frozen players should enter at. */
-room_vnum frozen_start_room = 0;
+room_vnum frozen_start_room = 2;
 
 /* Virtual numbers of donation rooms.  note: you must change code in do_drop of
  * act.item.c if you change the number of non-NOWHERE donation rooms. */
-room_vnum donation_room_1 = 0;
-room_vnum donation_room_2 = 0;
-room_vnum donation_room_3 = 0;
+room_vnum donation_room_1 = 2;
+room_vnum donation_room_2 = 2;
+room_vnum donation_room_3 = 2;
 
 /* GAME OPERATION OPTIONS */
 
