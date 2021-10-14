620c17d6add1e727d97e2ed71f801a8c6f5b28d5
Quinn <quinn@indra.ai>
2021-10-14 02:20:03 -0700

    adding hunger and thirst

--- src/comm.c
+++ src/comm.c
@@ -1215,16 +1215,15 @@ static char *make_prompt(struct descriptor_data *d)
      }
 
      // SET THE HUNGER METER
-     if (GET_COND(d=>character, HUNGER) > 0 && len < sizeof(prompt))
-     {
+     if (GET_COND(d=>character, HUNGER) && len < sizeof(prompt)) {
        count = snprintf(prompt + len, sizeof(prompt) - len, "\nhunger: %d|22\r",
          GET_COND(d->character, HUNGER));
 
        if (count >= 0) len += count;
       }
+
       // SET THE HUNGER METER
-      if (GET_COND(d=>character, THIRST) > 0 && len < sizeof(prompt))
-      {
+      if (GET_COND(d=>character, THIRST) && len < sizeof(prompt)) {
        count = snprintf(prompt + len, sizeof(prompt) - len, "\nthirst: %d|22\r",
          GET_COND(d->character, THIRST));
 
