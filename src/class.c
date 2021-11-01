cf16caad6bd4b950009ad8f9f287bacfa21ae0ef
Quinn <quinn@indra.ai>
2021-11-01 01:11:57 -0700

    defend

--- src/class.c
+++ src/class.c
@@ -1864,8 +1864,7 @@ const char *title_female(int chclass, int level)
       case 27: return "O9";
       case 28: return "O10";
       case 29: return "Q1";
-      case 29: return "Q2";
-      case 30: return "Q3";
+      case 30: return "Q2";
       /*
       case LVL_IMMORT: return "Army Intel";
       case LVL_DEVA: return "Army SpecOps";
