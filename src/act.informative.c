e80f7a9964f14435640844725e07c370cea00032
Quinn <quinn@indra.ai>
2021-11-04 23:13:23 -0700

    adv commands

--- src/act.informative.c
+++ src/act.informative.c
@@ -566,8 +566,12 @@ void look_at_room(struct char_data *ch, int ignore_brief)
       }
       else {
         /* send the room description */
-        send_to_char(ch, "\ntalk:#adv world:thegame %d\r"
+        send_to_char(ch, "\n::begin:room%d\r"
+          "\ntalk:#adv world:thegame %d\r"
+          "\n::end:room%d\r"
           "\nroom:%d\r",
+          GET_ROOM_VNUM(IN_ROOM(ch)),
+          GET_ROOM_VNUM(IN_ROOM(ch)),
           GET_ROOM_VNUM(IN_ROOM(ch)),
           GET_ROOM_VNUM(IN_ROOM(ch))
         );
