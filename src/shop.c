3a809f9e9cf10c1f541c9214d8ad36f00857ae8f
Quinn <quinn@indra.ai>
2021-11-18 20:31:20 -0800

    shop.c

--- src/shop.c
+++ src/shop.c
@@ -896,7 +896,7 @@ static void shopping_list(char *arg, struct char_data *ch, struct char_data *kee
 
   one_argument(arg, name);
 
-  len = strlcpy(buf,   "# Available Items\r\n", sizeof(buf));
+  len = strlcpy(buf,   "\n# Current Offerings\r", sizeof(buf));
   if (keeper->carrying)
     for (obj = keeper->carrying; obj; obj = obj->next_content)
       if (CAN_SEE_OBJ(ch, obj) && GET_OBJ_COST(obj) > 0) {
