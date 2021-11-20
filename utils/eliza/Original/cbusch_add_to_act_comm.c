51d934ac095bb01be359dfd8fb05f5c705bc9954
Quinn <quinn@indra.ai>
2021-11-20 01:39:48 -0800

    eliza

--- utils/eliza/Original/cbusch_add_to_act_comm.c
+++ utils/eliza/Original/cbusch_add_to_act_comm.c
@@ -2,21 +2,20 @@ Chris Busch
 
 The following code must be added to act_comm.c in the MERC distribution.  I
 encourage people to try to incorperate this code into their mud even if it
-is not a MERC. 
+is not a MERC.
 
 #error
 
 /*Add into file about line 604*/
-/*cbusch was here*/
 void chatperform(CHAR_DATA *ch,CHAR_DATA *victim,char* msg)
 { /*ch here should be a NPC, but its checked below*/
   char* reply;
-  if( !IS_NPC(ch) || (victim!=NULL && IS_NPC(victim)) ) 
+  if( !IS_NPC(ch) || (victim!=NULL && IS_NPC(victim)) )
     return;  /*ignores ch who are PCs and victims who are NPCs*/
   reply=dochat(ch->name,msg, victim ? victim->name : "you" );
   if(reply) {
     switch(reply[0]) {
-    case '\0': /* null msg*/ 
+    case '\0': /* null msg*/
       break;
     case '"' : /*do say*/
       do_say(ch,reply+1);
@@ -30,7 +29,7 @@ void chatperform(CHAR_DATA *ch,CHAR_DATA *victim,char* msg)
     default : /* is a say or tell*/
       if(victim == NULL )         do_say(ch,reply);
       else { /* do a tell  (this is always the case here)*/
-	act("$N tells you '$t'.",victim,reply,ch,TO_CHAR); 
+	act("$N tells you '$t'.",victim,reply,ch,TO_CHAR);
 	/*^^^^^^^^^^misnomer sends to victim*/
 	victim->reply=ch;
       }
@@ -38,17 +37,16 @@ void chatperform(CHAR_DATA *ch,CHAR_DATA *victim,char* msg)
   }
 }
 
-  
+
 /*about line 637*/
-/*cbusch was here*/
-void chatperformtoroom( char *txt, CHAR_DATA *ch) 
+void chatperformtoroom( char *txt, CHAR_DATA *ch)
 { /*ch here is the PC saying the phrase*/
 
   CHAR_DATA *vch;
   if( IS_NPC(ch)) return; /*we dont want NPCs trigger'ing events*/
 
   for ( vch = ch->in_room->people; vch != NULL; vch = vch->next_in_room )
-    if ( IS_NPC( vch ) && !( vch->pIndexData->progtypes & SPEECH_PROG ) 
+    if ( IS_NPC( vch ) && !( vch->pIndexData->progtypes & SPEECH_PROG )
 	&& IS_AWAKE(vch) )
       chatperform(vch,ch,txt);
   return;
@@ -61,9 +59,9 @@ void chatperformtoroom( char *txt, CHAR_DATA *ch)
 void do_tell( CHAR_DATA *ch, char *argument )
 {
   .........code removed........
-    /*cbusch 8/21/95 about line 716*/
+    /*8/21/95 about line 716*/
     chatperform(victim,ch,argument);
-    /*cbusch end*/
+    /*end*/
     return;
 }
 
@@ -71,9 +69,9 @@ void do_tell( CHAR_DATA *ch, char *argument )
 void do_reply( CHAR_DATA *ch, char *argument )
 {
     .........code removed.........
-    /*cbusch 8/21/95 about line 756*/
-    chatperform(victim,ch,argument); 
-    /*cbusch end*/
+    /*8/21/95 about line 756*/
+    chatperform(victim,ch,argument);
+    /*end*/
     return;
 }
 
@@ -82,8 +80,8 @@ void do_reply( CHAR_DATA *ch, char *argument )
 void do_emote( CHAR_DATA *ch, char *argument )
 {
   ....code deleted....
-    /*cbusch was here*/
+
     chatperformtoroom(argument,ch);
-    /*end cbusch was here*/
+    /*end was here*/
     return;
 }
