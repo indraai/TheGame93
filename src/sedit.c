6ec0da8b7cb9b4f0e0fd052949ab3418ec5f9e80
Quinn <quinn@indra.ai>
2021-11-21 03:37:57 -0800

    stuff

--- src/sedit.c
+++ src/sedit.c
@@ -477,6 +477,7 @@ void sedit_parse(struct descriptor_data *d, char *arg)
         write_to_output(d, "%s", confirm_msg);
 	     OLC_MODE(d) = SEDIT_CONFIRM_SAVESTRING;
       } else {
+        write_to_output(d, "\nsave:No changes made.")
         cleanup_olc(d, CLEANUP_ALL);
       }
       return;
