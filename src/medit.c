96aa832d6c40c5e9d718bc07e7ec02cbb7b668f0
Quinn <quinn@indra.ai>
2021-12-20 04:32:55 -0800

    medit vnum

--- src/medit.c
+++ src/medit.c
@@ -178,25 +178,12 @@ static void medit_setup_new(struct descriptor_data *d)
 {
   struct char_data *mob;
 
-  char agentnum[MAX_STRING_LENGTH];
-  char avatarstr[MAX_STRING_LENGTH];
-  char agentstr[MAX_STRING_LENGTH];
-
-  if (OLC_NUM(d) < 10) {
-    sprintf(agentnum, "000%d", OLC_NUM(d));
-  }
-  else if (OLC_NUM(d) < 100) {
-    sprintf(agentnum, "00%d", OLC_NUM(d));
-  }
-  else if (OLC_NUM(d) < 1000) {
-    sprintf(agentnum, "0%d", OLC_NUM(d));
-  }
-  else {
-    sprintf(agentnum, "%d", OLC_NUM(d));
-  }
+  int vnum
+  char avatarstr[MAX_STRING_LENGTH], agentstr[MAX_STRING_LENGTH];
+  
   /* create default strings with agent number in them; */
-  sprintf(avatarstr, "%s/avatar", agentnum);
-  sprintf(agentstr, "talk:#adv agent/thegame %s/look", agentnum);
+  sprintf(avatarstr, "%d/avatar", OLC_NUM(d));
+  sprintf(agentstr, "talk:#adv agent/thegame %d/look", OLC_NUM(d));
 
   /* Allocate a scratch Agent structure. */
   CREATE(mob, struct char_data, 1);
