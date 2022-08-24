0e01f13b57b8144130de29e15bf9aac87d35a861
Quinn <quinn@indra.ai>
2022-08-23 21:55:40 -0700

    comm.c edit

--- src/comm.c
+++ src/comm.c
@@ -1575,9 +1575,8 @@ static int new_descriptor(socket_t s)
   } else {
     greetsize = strlen(GREETINGS);
     write_to_output(newd, "%s", ProtocolOutput(newd, GREETINGS, &greetsize));
+    write_to_output(newd, "\ntrigger:login\r");
   }
-  write_to_output(newd, "\np:SHOULD TRIGGER LOGIN\r");
-  write_to_output(newd, "\ntrigger:login\r");
   return (0);
 }
 
